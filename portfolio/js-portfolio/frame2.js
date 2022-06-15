aa340fc40be026a784965ec1a84f98b4a1592f45
Acornicesei Mihaela Gabriela <esmeraldagabriela83@yahoo.com>
2022-06-15 17:35:02 +0300

    15 06 2022

---
+++ portfolio/js-portfolio/frame2.js
@@ -0,0 +1,30 @@
+console.log("frame2.js");
+
+let slideIndex2 = 1;
+showSlides2(slideIndex2);
+
+// Next/previous controls
+function plusSlides2(n) {
+  showSlides2(slideIndex2 += n);
+}
+
+// Thumbnail image controls
+function currentSlide2(n) {
+  showSlides2(slideIndex2 = n);
+}
+
+function showSlides2(n) {
+  let i;
+  let slides = document.getElementsByClassName("mySlides2");
+  let dots = document.getElementsByClassName("dot2");
+  if (n > slides.length) {slideIndex2 = 1}
+  if (n < 1) {slideIndex2 = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active2", "");
+  }
+  slides[slideIndex2-1].style.display = "block";
+  dots[slideIndex2-1].className += " active2";
+}
---
+++ portfolio/js-portfolio/frame5.js
@@ -0,0 +1,30 @@
+console.log("frame5.js");
+
+let slideIndex5 = 1;
+showSlides5(slideIndex5);
+
+// Next/previous controls
+function plusSlides5(n) {
+  showSlides5(slideIndex4 += n);
+}
+
+// Thumbnail image controls
+function currentSlide5(n) {
+  showSlides5(slideIndex5 = n);
+}
+
+function showSlides5(n) {
+  let i;
+  let slides = document.getElementsByClassName("mySlides5");
+  let dots = document.getElementsByClassName("dot5");
+  if (n > slides.length) {slideIndex5 = 1}
+  if (n < 1) {slideIndex5 = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active5", "");
+  }
+  slides[slideIndex5-1].style.display = "block";
+  dots[slideIndex5-1].className += " active5";
+}
